74f7cc734893663775101208bd8a1eea6c242176
Tymo <tr@krusemedien.com>
2019-01-11 21:21:57 +0100

    Startseite verlinken

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -1,6 +1,6 @@
 <header class="header">
 	<nav class="main-nav">
-		<h1>Töttchen</h1>
+		<h1><a href="<?php echo $site->url() ?>">Töttchen</a></h1>
 		<ul class="main-nav__list">
 		<?php foreach ($site->children()->listed() as $child): ?>
 			<li class="main-nav__item"><a href="<?php echo $child->url() ?>" class="main-nav__link"><?php echo $child->title()->html() ?></a></li>
